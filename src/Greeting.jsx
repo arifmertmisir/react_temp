53d757b525490f28403e4baf09ca215cd1ec86d9
Mert Misir <167693183+arifmertmisir@users.noreply.github.com>
2026-04-07 18:57:50 +0200

    Working with JSX

---
+++ src/Greeting.jsx
@@ -0,0 +1,26 @@
+function Greeting() {
+    return (
+        <div>
+            <h1>"I swear by my pretty floral bonnet, I will end you."</h1>
+            <h1>"In Icli köfte we trust!"</h1>
+            <h1>"In Salgam we trust!"</h1>
+            <h1>"In Tantuni we trust!"</h1>
+            <form>
+                <input type="text" />
+            </form>
+            <ul>
+                <li>item 1</li>
+                <li>item 2</li>
+            </ul>
+        </div>
+    );
+
+     
+}
+
+/*function Food(){
+    return <h1>"In Icli köfte we trust!"</h1>;
+}*/
+
+//export {Greeting, Food};
+export default Greeting;
